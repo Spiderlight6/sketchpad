81075b7e0b0edc64d0049d50ac152bac0b1db6cb
Spiderlight6 <170469796+Spiderlight6@user.noreply.github.com>
2024-08-25 03:52:17 +0200

    modified:   app.js 	modified:   index.html 	modified:   styles.css

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
 
 const container = document.querySelector("#container");
+const button = document.querySelector("#button");
+const containerSize = 900;
 
-
-// number of grid (Grid Size)
-const girdSize = 16;   // later we will take user input to set the value.
-
-
-const containerSize = 500;
-
-// set the size of the square divs 
-const squareSize = containerSize / girdSize; // they should have equal height and width
-
-for(let i = 0; i < girdSize * girdSize; i++){
-    const squareDivs = document.createElement("div");
-    squareDivs.setAttribute("class", "square");
-
-
-    // Set the size of the square divs dynamically 
-    squareDivs.style.width = `${squareSize}`
-    squareDivs.style.height = `${squareSize}`
+function createGrid(squaresPerSide) {
+    // Clear any existing squares
+    container.innerHTML = '';
     
+    // Calculate the size of each square
+    const squareSize = containerSize / squaresPerSide;
 
-    // Add the square divs into the container 
-    container.appendChild(squareDivs)
+    // Create the grid
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        container.appendChild(square);
+    }
 }
 
+createGrid(16);
 
 
+// changing the number of squares per side 
+
+button.addEventListener("click", () =>{
+    let newSize = parseInt(prompt("Enter the number of squares per side (1, 100) "), 10);
+    if (newSize > 0 && newSize <= 100){
+        createGrid(newSize);
+    }
+    else{
+        alert("Please enter a number between 1 and 100");
+    }
+})
+
+// create a function to generate random color
+
+function randomColor(){
+    let R = Math.floor(Math.random() * 256);
+    let G = Math.floor(Math.random() * 256);
+    let B = Math.floor(Math.random() * 256);
+
+    let randomcolor = "rgb(" + R + "," + G + "," + B + ")"; 
+    return randomcolor;
+}
+
+randomColor()
